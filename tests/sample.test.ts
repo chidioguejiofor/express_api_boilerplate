439dc6c3d79e1c1f1aef1ca50ced352ca81c673d
Chidiebere Ogujeiofor <chidioguejiofor@gmail.com>
2020-10-27 12:06:20 +0100

    chore(CD): updated docker setup
    - implemented continuous delivery using Circleci
    - added test coverage reporting using Istanbul's nyc
    - cleaned up sample tests
    
    [Finishes]

--- tests/sample.test.ts
+++ tests/sample.test.ts
@@ -4,8 +4,5 @@ describe('Setup Test suite', () => { // the tests container
     it('Checking that one is one', () => { // the single test
         expect(1, 'Value should be one').to.equal(1);
     });
-    it('Checking that one is 9', () => { // the single test
-        expect(1, 'Value should be 9').to.equal(9);
-    });
 
 });
