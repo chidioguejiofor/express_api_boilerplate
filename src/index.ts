6ae9eeff4477c8aaf6b180b385094803ef90a167
Chidiebere Ogujeiofor <chidioguejiofor@gmail.com>
2020-10-27 11:47:36 +0100

    ch(docker): adds dockerfile
    - permanently deletes .DS_Store from the project
    - adds Docker configs
    - updates Circleci configs to the app
    - corrects Redis host URL
    
    [Finishes]

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import server from "./app";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log(`Server Started at Port, ${PORT}`);
